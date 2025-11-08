d2f2a107363ff77e2f7a49734c412f3d4a61ab9b
Anup Maharjan <np03cs4s240186@heraldcollege.edu.np>
2025-11-08 21:25:09 +0545

    feat: add change password

--- NexusProcure.Application/Services/AuthService.cs
+++ NexusProcure.Application/Services/AuthService.cs
@@ -26,8 +26,11 @@ public class AuthService : IAuthService
             .Include(r => r.Role)
             .FirstOrDefaultAsync(u => u.Email == request.Email);
 
+        if (user == null)
+            return null;
+
         var passwordVerificationResult = new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password);
-        if (user == null || passwordVerificationResult == PasswordVerificationResult.Failed)
+        if (passwordVerificationResult == PasswordVerificationResult.Failed)
             return null;
 
         var token = JwtTokenGenerator.GenerateToken(user, _config["Jwt:Key"]!);
@@ -43,5 +46,32 @@ public class AuthService : IAuthService
             },
             Token = token
         };
+    }
+
+    public async Task<bool> ChangePasswordAsync(ChangePasswordRequest request)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+        if (user == null) return false;
+
+        
+        if (string.IsNullOrWhiteSpace(request.NewPassword) ||
+            request.NewPassword != request.ConfirmNewPassword)
+        {
+            return false;
+        }
+        
+        var hasher = new PasswordHasher<User>();
+        var verify = hasher.VerifyHashedPassword(user, user.PasswordHash, request.CurrentPassword);
+        if (verify == PasswordVerificationResult.Failed)
+            return false;
+
+        
+        if (hasher.VerifyHashedPassword(user, user.PasswordHash, request.NewPassword) == PasswordVerificationResult.Success)
+            return false;
+
+        
+        user.PasswordHash = hasher.HashPassword(user, request.NewPassword);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
