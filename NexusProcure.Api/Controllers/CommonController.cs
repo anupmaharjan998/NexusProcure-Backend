2781c23c63bf7b50634eed2d75a487dbeb58b51c
Anup Maharjan <np03cs4s240186@heraldcollege.edu.np>
2025-12-13 12:33:13 +0545

    Rename GetAll to GetAllCategory and Add to AddCategory

--- NexusProcure.Api/Controllers/CommonController.cs
+++ NexusProcure.Api/Controllers/CommonController.cs
@@ -15,14 +15,14 @@ public class CommonController : BaseApiController
     }
     
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> GetAllCategory()
     {
         var result = await _commonService.GetAllCategoryAsync();
         return Ok(result);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Add([FromBody] CategoryRequest request)
+    public async Task<IActionResult> AddCategory([FromBody] CategoryRequest request)
     {
         var result = await _commonService.AddCategoryAsync(request);
         return CreatedAtAction(nameof(GetAll), new { id = result.Id }, result);
