c5a70e0fdf4464de8eba7dd6defe670d7ef2b4c5
Anup Maharjan <np03cs4s240186@heraldcollege.edu.np>
2025-12-13 17:13:51 +0545

    Added Fields in DTO

--- NexusProcure.Core/DTOs/Vendor/VendorRequestDto.cs
+++ NexusProcure.Core/DTOs/Vendor/VendorRequestDto.cs
@@ -1,15 +1,21 @@
-﻿namespace NexusProcure.Core.DTOs.Vendor;
+﻿using NexusProcure.Core.Enums;
+
+namespace NexusProcure.Core.DTOs.Vendor;
 
 public class VendorRequestDto
 {
     public string VendorName { get; set; } = null!;
-    public string? CompanyName { get; set; }
+    public string CompanyName { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
+    public TaxType? TaxType { get; set; }
     public string? TaxId { get; set; }
-    public string? Category { get; set; }
+    public string Category { get; set; }
+    public Guid CategoryId { get; set; }
+    public string? BankName { get; set; }
+    public string? BankBranch { get; set; }
     public string? BankAccount { get; set; }
-    public string? PaymentTerms { get; set; }
+    public PaymentTerm PaymentTerms { get; set; }
     public string? Status { get; set; }
 }
